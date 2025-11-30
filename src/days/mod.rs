371a24db789d18e78abe83c3d0b233d49cdc27b5
Vigintillionn <yarne.ramakers@gmail.com>
2025-11-30 11:57:40 +0100

    chore: remove accidental push of day01

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -1,9 +1,7 @@
-pub mod day01;
 // [IMPORT_MARKER]
 
 pub fn get_solver(day: u8) -> Option<fn(&str) -> crate::Solution> {
     match day {
-        1 => Some(day01::solve),
         // [MATCH_MARKER]
         _ => None,
     }
